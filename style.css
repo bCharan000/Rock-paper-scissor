066794f69a1bd059b7f5c61507084a8fd5d2f6d0
bCharan000 <155760958+bCharan000@users.noreply.github.com>
2024-01-07 00:00:36 +0530

    Update style.css

---
+++ style.css
@@ -0,0 +1,71 @@
+*{
+    padding: 0;
+    margin: 0;
+    text-align:center ;
+}
+
+#heading{
+    background-color: rgb(40, 55, 71);
+    color: rgb(243, 245, 245);
+    height: 5rem;
+    text-align: center;
+     line-height: 5rem; /*to get the text center height wise */
+}
+#options{
+    display: flex;
+    justify-content: center;
+    gap: 3rem;
+    margin-top: 4rem;
+    
+
+.choise{
+    height: 172px;
+    width: 172px;
+    border-radius: 50%;
+    justify-items: center;
+    align-items: center;
+}
+
+}
+.choise:hover{
+background-color: rgb(100, 167, 239);
+opacity: .8;
+cursor: pointer;
+
+}
+
+img{
+    height: 170px;
+    width: 170px;
+    border-radius: 50%;
+    object-fit: cover;/*to get a better img*/
+   
+}
+#score{
+    display: flex;
+    justify-content: center;
+    margin-top: 5rem;
+    gap: 2rem;
+
+}
+.score{
+    font-size: 4rem;
+    font-weight: bolder;
+    
+}
+p{
+    margin-top: 1rem;
+    font-size: 2rem;
+
+}
+h3{
+    height: 2rem;
+    line-height: 2rem;
+    display: inline-block;
+    margin-top: 2rem;
+    background-color: green;
+    color: white;
+    padding: 1rem;
+    border-radius: 2rem;
+}
+
